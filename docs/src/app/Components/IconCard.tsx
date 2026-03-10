9002bbca4d28e32aa0030bb6d8cbed52634c3613
Yannick <yannick.franssen@outlook.com>
2026-03-10 23:40:35 +0100

    Fix icon card clickable area

--- docs/src/app/Components/IconCard.tsx
+++ docs/src/app/Components/IconCard.tsx
@@ -6,11 +6,12 @@ import Amicon from '@studio384/amicons';
 
 export default function IconCard({ icon }: { icon: ILibraryIcon }) {
   return (
-    <div className="relative flex flex-col items-center justify-center gap-3 rounded-md bg-neutral-100 p-4 text-black focus-within:bg-violet-200 focus-within:text-violet-700 hover:bg-violet-200 hover:text-violet-700">
+    <NavLink
+      to={`/icons/${icon.slug}`}
+      className="relative flex flex-col items-center justify-center gap-3 rounded-md bg-neutral-100 p-4 text-black focus-within:bg-violet-200 focus-within:text-violet-700 hover:bg-violet-200 hover:text-violet-700"
+    >
       <Amicon icon={icon.icon} className="mt-1 text-3xl" />
-      <NavLink to={`/icons/${icon.slug}`} className="max-w-full text-inherit">
-        <div className="truncate font-mono text-xs text-nowrap text-neutral-700">{icon.slug}</div>
-      </NavLink>
-    </div>
+      <span className="truncate font-mono text-xs text-nowrap text-neutral-700">{icon.slug}</span>
+    </NavLink>
   );
 }
