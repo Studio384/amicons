d4e314b971787f60fe3b0fe29ddf5afc58c4b434
Yannick <yannick.franssen@outlook.com>
2026-03-10 23:36:46 +0100

    Migrate the icon card to Tailwind

--- docs/src/app/Components/IconCard.tsx
+++ docs/src/app/Components/IconCard.tsx
@@ -1,55 +1,16 @@
 import { NavLink } from 'react-router';
 
-import { Box, Link, Sheet, Stack, Typography } from '@mui/joy';
-
 import { ILibraryIcon } from '@/types';
 
 import Amicon from '@studio384/amicons';
 
 export default function IconCard({ icon }: { icon: ILibraryIcon }) {
   return (
-    <Sheet
-      key={icon.slug}
-      sx={{
-        gap: 0,
-        borderRadius: 'sm',
-        color: 'text.primary',
-        bgcolor: 'neutral.50',
-        '&:hover, &:focus-within': {
-          color: 'primary.700',
-          backgroundColor: 'primary.100'
-        }
-      }}
-    >
-      <Stack gap={1} justifyContent="center" alignItems="center" sx={{ pt: 2.5, pb: 1.5 }}>
-        <Box sx={{ height: 28, width: 28, fontSize: 28 }} lineHeight="1rem">
-          <Amicon icon={icon.icon} />
-        </Box>
-        <Link
-          overlay
-          component={NavLink}
-          to={`/icons/${icon.slug}`}
-          underline="none"
-          color="neutral"
-          sx={{
-            maxWidth: 'calc(100% - 16px)'
-          }}
-        >
-          <Typography
-            noWrap
-            level="body-sm"
-            sx={{
-              px: 0.5,
-              py: 0.25,
-              borderRadius: 'sm',
-              fontSize: 12,
-              fontFamily: 'SFMono-Regular,Menlo,Monaco,Consolas,Liberation Mono,Courier New,monospace'
-            }}
-          >
-            {icon.slug}
-          </Typography>
-        </Link>
-      </Stack>
-    </Sheet>
+    <div className="relative flex flex-col items-center justify-center gap-3 rounded-md bg-neutral-100 p-4 text-black focus-within:bg-violet-200 focus-within:text-violet-700 hover:bg-violet-200 hover:text-violet-700">
+      <Amicon icon={icon.icon} className="mt-1 text-3xl" />
+      <NavLink to={`/icons/${icon.slug}`} className="max-w-full text-inherit">
+        <div className="truncate font-mono text-xs text-nowrap text-neutral-700">{icon.slug}</div>
+      </NavLink>
+    </div>
   );
 }
