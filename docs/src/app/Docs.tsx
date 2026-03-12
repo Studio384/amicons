8e626cf6b414178883541b013289684c08383e72
Yannick <yannick.franssen@outlook.com>
2026-03-12 23:03:10 +0100

    Update docs layout

--- docs/src/app/Docs.tsx
+++ docs/src/app/Docs.tsx
@@ -1,7 +1,5 @@
 import { Outlet } from 'react-router';
 
-import { Box, Stack } from '@mui/joy';
-
 import Header from '@/design/layout/LayoutElements/Header';
 
 import DocsNavigation from './Docs/Navigation';
@@ -13,20 +11,14 @@ export default function Docs() {
         <h1 className="font-display py-2 text-5xl font-medium text-black">Documentation</h1>
       </Header>
       <div className="container m-auto my-8 max-w-7xl px-4">
-        <Box
-          sx={{
-            display: 'grid',
-            gridTemplateColumns: '220px auto',
-            gap: 3
-          }}
-        >
-          <Box sx={{ position: 'sticky', top: 58 + 32, overflow: 'auto', maxHeight: 'calc(100dvh - 58px - 32px)', alignSelf: 'flex-start' }}>
+        <div className="grid grid-cols-[220px_auto] gap-4">
+          <div className="max-h-[calc(100dvh-90px) sticky top-22.5 self-start overflow-auto">
             <DocsNavigation />
-          </Box>
-          <Stack gap={4}>
+          </div>
+          <div>
             <Outlet />
-          </Stack>
-        </Box>
+          </div>
+        </div>
       </div>
     </>
   );
