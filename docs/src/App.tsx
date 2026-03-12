d9b8075f524a346bb6c494cba60e7cb951729eb3
Yannick <yannick.franssen@outlook.com>
2026-03-12 23:24:20 +0100

    Cleanup router, support Layout for children for root error

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from 'react-router';
 import { CssBaseline, CssVarsProvider, GlobalStyles } from '@mui/joy';
 
 import joyTheme from './design/joyTheme';
-import router from './Router';
+import { router } from './Router';
 
 export default function App() {
   return (
--- docs/src/design/layout/Layout.tsx
+++ docs/src/design/layout/Layout.tsx
@@ -1,15 +1,16 @@
+import { PropsWithChildren } from 'react';
 import { Outlet } from 'react-router';
 
 import ScrollToTop from '../components/ScrollToTop';
 import Footer from './LayoutElements/Footer';
 import Navbar from './LayoutElements/Navbar';
 
-export default function Layout() {
+export default function Layout({ children }: PropsWithChildren) {
   return (
     <div className="-mt-23">
       <ScrollToTop />
       <Navbar />
-      <Outlet />
+      {children ? children : <Outlet />}
       <Footer />
     </div>
   );
