2a5c2bebabd5c99524ed071cc16a08df33673d2b
Yannick <yannick.franssen@outlook.com>
2026-03-14 22:36:52 +0100

    Improve search performance, create better hook

--- docs/src/app/Icons.tsx
+++ docs/src/app/Icons.tsx
@@ -1,5 +1,4 @@
-import { useCallback, useMemo } from 'react';
-import { useSearchParams } from 'react-router';
+import { useMemo } from 'react';
 
 import categories from '@/data/categories';
 import icons from '@/data/icons';
@@ -8,6 +7,7 @@ import { IconCard } from '@/design/components/IconCard';
 import { Pagination } from '@/design/components/Pagination';
 import { Search } from '@/design/components/Search';
 import Header from '@/design/layout/LayoutElements/Header';
+import { useFilters } from '@/hooks/useFilters';
 import useSearch from '@/hooks/useSearch';
 import { ILibraryIcon } from '@/types';
 
@@ -15,75 +15,19 @@ import Amicon, { aiFilterXmark, aiXmark } from '@studio384/amicons';
 import clsx from 'clsx';
 
 export default function Icons() {
-  const [searchParams, setSearchParams] = useSearchParams();
-
-  const [searchCategories, searchQuery, searchPage]: [string[], string, number] = useMemo(() => {
-    const categories = searchParams.get('category');
-    const query = searchParams.get('search');
-    const page = Number(searchParams.get('page') ?? 1);
-
-    return [categories?.split(',').filter((item) => item !== '') ?? [], query ?? '', page ?? 1];
-  }, [searchParams]);
+  const filters = useFilters();
 
   const searchableList = useMemo(() => {
-    if (searchCategories.length >= 1) {
-      return icons.filter((icon) => searchCategories.every((_searchCategory) => icon.categories.includes(_searchCategory as never)));
+    if (filters.query.categories.length >= 1) {
+      return icons.filter((icon) => filters.query.categories.every((_searchCategory) => icon.categories.includes(_searchCategory as never)));
     }
 
     return icons;
-  }, [searchCategories]);
+  }, [filters.query.categories]);
 
-  const { result } = useSearch(searchableList, ['slug', 'tags'], searchQuery);
+  console.log(searchableList);
 
-  // c: categories
-  // q: query
-  // p: page
-  const setSearchQuery = useCallback(
-    (type: 'q' | 'c' | 'p', value: string | number) => {
-      let search = searchParams.get('search');
-      let page = Number(searchParams.get('page'));
-      let category =
-        searchParams
-          .get('category')
-          ?.split(',')
-          .filter((item) => item !== '') ?? [];
-
-      switch (type) {
-        case 'c': {
-          if (typeof value === 'number') return;
-
-          if (category.includes(value)) {
-            category = category.filter((item) => item !== value);
-          } else {
-            category.push(value);
-          }
-
-          page = 1; // Always reset page
-          break;
-        }
-        case 'q': {
-          if (typeof value === 'number') return;
-
-          search = value;
-          page = 1; // Always reset page
-          break;
-        }
-        case 'p': {
-          if (typeof value === 'string') return;
-
-          page = value;
-          break;
-        }
-      }
-
-      setSearchParams({
-        page: (page || 1).toString(),
-        search: search ?? '',
-        category: category.join(',') ?? ''
-      });
-    },
-    [searchParams, setSearchParams]
-  );
+  const { result } = useSearch(searchableList, ['slug', 'tags'], filters.query.search);
 
   return (
     <>
@@ -100,13 +44,13 @@ export default function Icons() {
                 return (
                   <button
                     key={_category.slug}
-                    onClick={() => setSearchQuery('c', _category.slug)}
-                    data-selected={searchCategories.includes(_category.slug) || undefined}
+                    onClick={() => filters.toggleCategory(_category.slug)}
+                    data-selected={filters.query.categories.includes(_category.slug) || undefined}
                     data-noicons={categoryIcons.length === 0 ? true : undefined}
                     className={clsx(
                       'group grid h-8 grid-cols-[min-content_auto_min-content] items-center gap-2 rounded-sm px-2.5 text-start text-sm hover:cursor-pointer hover:bg-violet-200 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-violet-500 data-selected:focus-visible:outline-violet-700',
                       {
-                        'bg-violet-500 text-white hover:bg-violet-600': searchCategories.includes(_category.slug)
+                        'bg-violet-500 text-white hover:bg-violet-600': filters.query.categories.includes(_category.slug)
                       }
                     )}
                   >
@@ -123,46 +67,41 @@ export default function Icons() {
               <div className="flex items-baseline gap-2">
                 <h2 className="font-display text-3xl font-medium">{result.length} icons</h2>
                 <span className="text-zinc-600">
-                  Page {searchPage} of {Math.ceil(result.length / 96)}
+                  Page {filters.query.page} of {Math.ceil(result.length / 96)}
                 </span>
               </div>
 
               <div className="flex gap-1">
-                <Search placeholder="Search" value={searchQuery} onValueChange={(value) => setSearchQuery('q', value)} />
+                <Search placeholder="Search" value={filters.searchValue} onValueChange={(value) => filters.setSearch(value)} />
                 <Button
                   icon
                   variant="secondary"
-                  disabled={searchQuery === '' && searchCategories.length === 0}
-                  onClick={() => {
-                    setSearchParams({
-                      search: '',
-                      category: ''
-                    });
-                  }}
+                  disabled={filters.searchValue === '' && filters.query.categories.length === 0}
+                  onClick={() => filters.resetQuery()}
                 >
                   <Amicon icon={aiFilterXmark} />
                 </Button>
               </div>
             </div>
-            {(searchQuery || searchCategories.length >= 1) && (
+            {(filters.query.search || filters.query.categories.length >= 1) && (
               <div className="flex gap-1">
-                {searchQuery && (
+                {filters.query.search && (
                   <div className="font-display flex items-center gap-1 rounded-full bg-zinc-100 py-1 ps-2.5 pe-1 text-sm">
-                    "{searchQuery}"
+                    "{filters.query.search}"
                     <button
                       className="text-md flex size-6 cursor-pointer items-center justify-center rounded-full bg-transparent hover:bg-zinc-300"
-                      onClick={() => setSearchQuery('q', '')}
+                      onClick={() => filters.setSearch('')}
                     >
                       <Amicon icon={aiXmark} /> <span className="sr-only">Delete category</span>
                     </button>
                   </div>
                 )}
-                {searchCategories.map((category) => (
+                {filters.query.categories.map((category) => (
                   <div key={category} className="font-display flex items-center gap-1 rounded-full bg-zinc-100 py-1 ps-2.5 pe-1 text-sm">
                     {category}
                     <button
                       className="text-md flex size-6 cursor-pointer items-center justify-center rounded-full bg-transparent hover:bg-zinc-300"
-                      onClick={() => setSearchQuery('c', category)}
+                      onClick={() => filters.toggleCategory(category)}
                     >
                       <Amicon icon={aiXmark} /> <span className="sr-only">Delete category</span>
                     </button>
@@ -171,12 +110,12 @@ export default function Icons() {
               </div>
             )}
             <div className="grid grid-cols-[repeat(auto-fill,minmax(min(9rem,100%),1fr))] gap-2">
-              {result.slice((searchPage - 1) * 96, searchPage * 96).map((icon: ILibraryIcon) => (
+              {result.slice((filters.query.page - 1) * 96, filters.query.page * 96).map((icon: ILibraryIcon) => (
                 <IconCard key={icon.slug} icon={icon} />
               ))}
             </div>
 
-            {result.length > 0 && <Pagination count={Math.ceil(result.length / 96)} page={searchPage} onChange={(_, page) => setSearchQuery('p', page)} />}
+            {result.length > 0 && <Pagination count={Math.ceil(result.length / 96)} page={filters.query.page} onChange={(_, page) => filters.setPage(page)} />}
           </div>
         </div>
       </div>
