d27ad514df5aff37a6734345d1d8f27a6e2ae62b
Yannick <yannick.franssen@outlook.com>
2026-03-14 13:56:19 +0100

    Remove Joy entirely

--- docs/src/App.tsx
+++ docs/src/App.tsx
@@ -1,22 +1,7 @@
 import { RouterProvider } from 'react-router';
 
-import { CssBaseline, CssVarsProvider, GlobalStyles } from '@mui/joy';
-
-import joyTheme from './design/joyTheme';
 import { router } from './Router';
 
 export default function App() {
-  return (
-    <CssVarsProvider theme={joyTheme} defaultMode="light">
-      <CssBaseline />
-      <GlobalStyles
-        styles={{
-          '*': {
-            scrollBehavior: 'smooth'
-          }
-        }}
-      />
-      <RouterProvider router={router} />
-    </CssVarsProvider>
-  );
+  return <RouterProvider router={router} />;
 }
