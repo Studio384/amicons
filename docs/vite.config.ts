aff5192a7d58578eea53074182b612664be902f7
Yannick <yannick.franssen@outlook.com>
2026-04-01 00:26:48 +0200

    Fix docs not properly using ReactCompiler

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -2,12 +2,14 @@ import tailwindcss from "@tailwindcss/vite";
 import { devtools } from "@tanstack/devtools-vite";
 import react from "@vitejs/plugin-react-swc";
 import * as path from "path";
+import ReactCompiler from "babel-plugin-react-compiler";
 import { visualizer } from "rollup-plugin-visualizer";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
+    [ReactCompiler],
     tailwindcss(),
     react(),
     devtools(),
