85678954c616f2d3b5651dfb5403c4b4a6b3b0c0
Yannick <yannick.franssen@outlook.com>
2026-04-06 13:22:13 +0200

    Move changelog

--- docs/src/app/Docs/Navigation.tsx
+++ docs/src/app/Docs/Navigation.tsx
@@ -19,13 +19,18 @@ export default function DocsNavigation() {
 
   const pages = [
     {
-      title: "Get started",
+      title: "Introduction",
       icon: aiAmicons,
       pages: [
         {
           title: "Installation",
           icon: aiAmicons,
           link: "/docs/installation",
+        },
+        {
+          title: "Changelog",
+          icon: aiStar,
+          link: "/changelog",
         },
       ],
     },
@@ -65,17 +70,6 @@ export default function DocsNavigation() {
         },
       ],
     },
-    {
-      title: "More",
-      icon: aiStar,
-      pages: [
-        {
-          title: "Changelog",
-          icon: aiStar,
-          link: "/changelog",
-        },
-      ],
-    },
   ];
 
   return (
@@ -94,9 +88,8 @@ export default function DocsNavigation() {
                 className={clsx(
                   "group flex h-8 items-center gap-2 rounded-sm px-2.5 text-start text-sm hover:cursor-pointer hover:bg-violet-200 focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-violet-500 data-selected:focus-visible:outline-violet-700 dark:hover:bg-violet-800",
                   {
-                    "bg-violet-500 text-white hover:bg-violet-600 dark:bg-violet-800": location.pathname.includes(
-                      page.link,
-                    ),
+                    "bg-violet-500 text-white hover:bg-violet-600 dark:bg-violet-800":
+                      location.pathname.includes(page.link),
                   },
                 )}
               >
