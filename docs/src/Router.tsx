f71bb04bbf89a5526d6b8b216631a98e3b4aad6b
Studio384 <yannick.franssen@outlook.com>
2026-04-11 11:58:28 +0200

    Fix releases layout

--- docs/src/Router.tsx
+++ docs/src/Router.tsx
@@ -9,13 +9,14 @@ import PageBounce from "./app/Docs/pages/Bounce.mdx";
 import PageFade from "./app/Docs/pages/Fade.mdx";
 import PageFlip from "./app/Docs/pages/Flip.mdx";
 import PageInstallation from "./app/Docs/pages/Installation.mdx";
-import Releases from "./app/Docs/pages/Releases.mdx";
+import PageReleases from "./app/Docs/pages/Releases.mdx";
 import PageRotate from "./app/Docs/pages/Rotate.mdx";
 import PageSpin from "./app/Docs/pages/Spin.mdx";
 import Error from "./app/Error";
 import Icon from "./app/Icon";
 import Icons from "./app/Icons";
 import Layout from "./design/layout/Layout";
+import Releases from "./app/Releases";
 
 export const router = createHashRouter([
   {
@@ -43,7 +44,6 @@ export const router = createHashRouter([
           { path: "flip", Component: PageFlip },
           { path: "beat", Component: PageBeat },
           { path: "fade", Component: PageFade },
-          { path: "releases", Component: Releases },
         ],
       },
       {
@@ -56,9 +56,9 @@ export const router = createHashRouter([
       },
       {
         path: "/releases",
-        Component: Docs,
+        Component: Releases,
         errorElement: <Error />,
-        children: [{ index: true, Component: Releases }],
+        children: [{ index: true, Component: PageReleases }],
       },
     ],
   },
