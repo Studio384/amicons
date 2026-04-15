c96aa31af91e15d639d826f3bc1850304c446c7f
Studio384 <yannick.franssen@outlook.com>
2026-04-15 23:08:55 +0200

    Minor menu cleanup

--- docs/src/neo/design/layouts/Menu.tsx
+++ docs/src/neo/design/layouts/Menu.tsx
@@ -25,7 +25,9 @@ export default function Menu(props: React.HTMLAttributes<HTMLDivElement>) {
   const location = useLocation();
   const navigate = useNavigate();
   const isDocumentationRoute = location.pathname.startsWith("/neo/documentation");
-  const [view, setView] = useState<"primary" | "documentation">(isDocumentationRoute ? "documentation" : "primary");
+  const [view, setView] = useState<"primary" | "documentation">(
+    isDocumentationRoute ? "documentation" : "primary",
+  );
   const previousPathname = useRef(location.pathname);
 
   useEffect(() => {
@@ -71,7 +73,12 @@ export default function Menu(props: React.HTMLAttributes<HTMLDivElement>) {
                     const isActive = location.pathname === page.path;
 
                     return (
-                      <NavigationMenu.Item key={page.path} to={page.path} size="sm" active={isActive}>
+                      <NavigationMenu.Item
+                        key={page.path}
+                        to={page.path}
+                        size="sm"
+                        active={isActive}
+                      >
                         <Amicon icon={page.icon} />
                         {page.title}
                       </NavigationMenu.Item>
@@ -94,7 +101,10 @@ export default function Menu(props: React.HTMLAttributes<HTMLDivElement>) {
               <Amicon icon={aiIcons} /> Icons
             </NavigationMenu.Item>
 
-            <NavigationMenu.ActionItem active={isDocumentationRoute} onClick={openDocumentationNavigation}>
+            <NavigationMenu.ActionItem
+              active={isDocumentationRoute}
+              onClick={openDocumentationNavigation}
+            >
               <Amicon icon={aiBook} />
               Documentation
               <Amicon icon={aiChevronRight} />
@@ -105,23 +115,28 @@ export default function Menu(props: React.HTMLAttributes<HTMLDivElement>) {
               News
             </NavigationMenu.Item>
 
-            <NavigationMenu.Item to="/neo/releases" active={location.pathname.includes("/neo/releases")}>
+            <NavigationMenu.Item
+              to="/neo/releases"
+              active={location.pathname.includes("/neo/releases")}
+            >
               <Amicon icon={aiRocket} />
               Releases
-            </NavigationMenu.Item>
-
-            <Separator orientation="horizontal" className="my-2 h-px bg-zinc-950/5 dark:bg-white/10" />
-
-            <NavigationMenu.Item to="/neo/error" active={location.pathname.includes("/neo/error")}>
-              <Amicon icon={aiCircleQuestion} />
-              Error
             </NavigationMenu.Item>
           </NavigationMenu.List>
         )}
 
         <Separator orientation="horizontal" className="my-2 h-px bg-zinc-950/5 dark:bg-white/10" />
 
-        <div className="mt-4 flex shrink-0 flex-row gap-1">
+        <NavigationMenu.List>
+          <NavigationMenu.Item to="/neo/error" active={location.pathname.includes("/neo/error")}>
+            <Amicon icon={aiCircleQuestion} />
+            Error
+          </NavigationMenu.Item>
+        </NavigationMenu.List>
+
+        <Separator orientation="horizontal" className="my-2 h-px bg-zinc-950/5 dark:bg-white/10" />
+
+        <div className="flex shrink-0 flex-row gap-1">
           <a
             href="https://patreon.com/amicons"
             target="_blank"
@@ -162,7 +177,10 @@ export default function Menu(props: React.HTMLAttributes<HTMLDivElement>) {
           href="https://studio384.be"
           className="font-dev text-2xl font-semibold hover:underline hover:decoration-[#78b500] hover:decoration-1 hover:underline-offset-1"
         >
-          Studio <span className="bg-linear-to-r from-[#78b500] to-[#00b573] bg-clip-text text-transparent">384</span>
+          Studio{" "}
+          <span className="bg-linear-to-r from-[#78b500] to-[#00b573] bg-clip-text text-transparent">
+            384
+          </span>
         </a>
         <p className="font-display text-xs text-zinc-500">&copy; 2021-2026</p>
       </div>
